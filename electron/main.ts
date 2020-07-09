3ea2e44d2ee1b3ed8001c7fc9698276f5d99e731
Leonardo Lopes <leonardolopesinf@gmail.com>
2020-07-08 22:22:02 -0300

    Update initial width

--- electron/main.ts
+++ electron/main.ts
@@ -5,9 +5,9 @@ import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electro
 
 let mainWindow: Electron.BrowserWindow | null
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 1100,
+    width: 1000,
     height: 700,
     backgroundColor: '#24292e',
     frame: false,
@@ -39,11 +39,11 @@ app.on('ready', createWindow)
   .then(() => {
     if (process.env.NODE_ENV === 'development') {
       installExtension(REACT_DEVELOPER_TOOLS)
-          .then((name) => console.log(`Added Extension:  ${name}`))
-          .catch((err) => console.log('An error occurred: ', err));
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log('An error occurred: ', err));
       installExtension(REDUX_DEVTOOLS)
-          .then((name) => console.log(`Added Extension:  ${name}`))
-          .catch((err) => console.log('An error occurred: ', err));
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log('An error occurred: ', err));
     }
   })
 app.allowRendererProcessReuse = true
